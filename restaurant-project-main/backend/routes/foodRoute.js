6f485916aa9720d4cfe41138fe66519eb4b8bdf1
Sakshimalusare <165875531+Sakshimalusare@users.noreply.github.com>
2025-01-29 18:52:33 +0530

    Update foodRoute.js

--- restaurant-project-main/backend/routes/foodRoute.js
+++ restaurant-project-main/backend/routes/foodRoute.js
@@ -1,35 +1,23 @@
 import express from "express";
-import { addFood,listFood,removeFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
+import authMiddleware from "../middleware/auth.js";
 
 const foodRouter = express.Router();
 
-// Multer setup for file upload
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads"); // Specify the uploads directory
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`); // Create unique file name
-    },
-});
+// Image Storage Engine
 
-const upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png|gif/;
-        const extname = fileTypes.test(file.originalname.toLowerCase());
-        const mimetype = fileTypes.test(file.mimetype);
+const storage= multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
 
-        if (mimetype && extname) {
-            return cb(null, true);
-        } else {
-            cb(new Error("Only images are allowed"));
-        }
-    },
-});
+const upload= multer({storage:storage})
 
-foodRouter.post("/add", upload.single("image"), addFood);
-foodRouter.get("/list",listFood)
-foodRouter.delete("/remove/",removeFood)
+foodRouter.post("/add",upload.single("image"),authMiddleware,addFood);
+foodRouter.get("/list",listFood);
+foodRouter.post("/remove",authMiddleware,removeFood);
+
 export default foodRouter;
